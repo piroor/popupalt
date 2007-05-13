908f97cb94438ae082b19f8bd5b0f8d0eb60d1b9
piro <piro@599a83e7-65a4-db11-8015-0010dcdd6dc2>
2007-05-13 20:42:44 +0000

    説明文をローカライズした
    git-svn-id: https://www.cozmixng.org/repos/piro/popupalt/trunk@572 599a83e7-65a4-db11-8015-0010dcdd6dc2

---
+++ defaults/preferences/popupalt.js
@@ -0,0 +1,2 @@
+pref("extensions.{61FD08D8-A2CB-46c0-B36D-3F531AC53C12}.name", "chrome://popupalt/locale/popupalt.properties");
+pref("extensions.{61FD08D8-A2CB-46c0-B36D-3F531AC53C12}.description", "chrome://popupalt/locale/popupalt.properties");
