64427c9fc0bec47d5df8b38138f0b989ca70f8b4
piro <piro@599a83e7-65a4-db11-8015-0010dcdd6dc2>
2009-11-02 09:35:24 +0000

    use xpath
    git-svn-id: https://www.cozmixng.org/repos/piro/popupalt/trunk@5342 599a83e7-65a4-db11-8015-0010dcdd6dc2

--- defaults/preferences/popupalt.js
+++ defaults/preferences/popupalt.js
@@ -1,2 +1,2 @@
-pref("extensions.{61FD08D8-A2CB-46c0-B36D-3F531AC53C12}.name", "chrome://popupalt/locale/popupalt.properties");
-pref("extensions.{61FD08D8-A2CB-46c0-B36D-3F531AC53C12}.description", "chrome://popupalt/locale/popupalt.properties");
+pref("browser.chrome.tooltips.attrlist.enabled", false);
+pref("browser.chrome.tooltips.attrlist", "alt|src|data|title|href|cite|action|onclick|onmouseover|onsubmit");
