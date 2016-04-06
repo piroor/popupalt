262d094ad24b1c6ab97726b3018ee17a325acd96
YUKI Hiroshi <shimoda@clear-code.com>
2016-04-06 17:26:03 +0900

    Support attribute values for replace targets

--- options/localizeLabels.js
+++ options/localizeLabels.js
@@ -6,19 +6,40 @@
 
 document.addEventListener('DOMContentLoaded', function onReady() {
 	document.removeEventListener('DOMContentLoaded', onReady);
-	var textNodes = document.evaluate(
+
+	function apply(aString)
+	{
+		return aString.replace(/__MSG_(.+?)__/g, function(aMatched) {
+			var key = aMatched.slice(6, -2);
+			return chrome.i18n.getMessage(key);
+		});
+	}
+
+	var texts = document.evaluate(
 			'descendant::text()[contains(self::text(), "__MSG_")]',
 			document,
 			null,
 			XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
 			null
 		);
-	for (let i = 0, maxi = textNodes.snapshotLength; i < maxi; i++)
+	for (let i = 0, maxi = texts.snapshotLength; i < maxi; i++)
 	{
-		let text = textNodes.snapshotItem(i);
-		text.nodeValue = text.nodeValue.replace(/__MSG_(.+?)__/g, function(aMatched) {
-			var key = aMatched.slice(6, -2);
-			return chrome.i18n.getMessage(key);
-		});
+		let text = texts.snapshotItem(i);
+		text.nodeValue = apply(text.nodeValue);
+	}
+
+	var attributes = document.evaluate(
+			'descendant::*/attribute::*[contains(., "__MSG_")]',
+			document,
+			null,
+			XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+			null
+		);
+	console.log('attributes: '+attributes.snapshotLength);
+	for (let i = 0, maxi = attributes.snapshotLength; i < maxi; i++)
+	{
+		let attribute = attributes.snapshotItem(i);
+		console.log(attribute);
+		attribute.nodeValue = apply(attribute.nodeValue);
 	}
 });
