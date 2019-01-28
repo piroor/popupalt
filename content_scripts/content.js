4406593b41808a4ca60b7aae0122e9d1a53d9ee9
YUKI Hiroshi <shimoda@clear-code.com>
2019-01-28 14:52:19 +0900

    Bump copyright year to 2019

--- content_scripts/content.js
+++ content_scripts/content.js
@@ -14,7 +14,7 @@
  * The Original Code is the Popup ALT Attribute.
  *
  * The Initial Developer of the Original Code is YUKI "Piro" Hiroshi.
- * Portions created by the Initial Developer are Copyright (C) 2002-2016
+ * Portions created by the Initial Developer are Copyright (C) 2002-2019
  * the Initial Developer. All Rights Reserved.
  *
  * Contributor(s): YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
